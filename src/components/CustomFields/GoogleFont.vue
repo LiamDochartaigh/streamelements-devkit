0754220ec08d51cab4c48e627bd24a4cfe1cda01
Hexeum <info@hexeum.net>
2025-08-21 18:32:56 +0200

    google fonts dropdown

--- src/components/CustomFields/GoogleFont.vue
+++ src/components/CustomFields/GoogleFont.vue
@@ -1,22 +1,33 @@
 <template>
     <div>
-        <div>{{  fieldData?.label}}</div>
+        <div>{{ fieldData?.label }}</div>
         <input type="text" :value="props.fieldData?.value" @input="TextChanged" />
     </div>
-    </template>
-    
-    <script setup lang="ts">    
-    const props = defineProps({
-        fieldData: Object
-    });
-    
-    const emit = defineEmits(['input']);
-    
-    function TextChanged(event: Event){
-        const target = event.target as HTMLInputElement;
-        emit('input', target.value);
-    }
-    </script>
-    
-    <style>
+</template>
+
+<script setup lang="ts">
+import fonts from "@/assets/fonts.json";
+import webfont from 'webfontloader';
+
+interface GoogleFont {
+    family: string;
+    category: string;
+    variants: string[];
+}
+
+const props = defineProps({
+    fieldData: Object
+});
+
+const googleFonts: GoogleFont[] = fonts.map((font) => ({ family: font.family, category: font.category, variants: font.variants }));
+
+const emit = defineEmits(['input']);
+
+function TextChanged(event: Event) {
+    const target = event.target as HTMLInputElement;
+    emit('input', target.value);
+}
+</script>
+
+<style>
     </style>
